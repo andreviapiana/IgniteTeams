a4511dc32ced12e1aa5c44ae9b3c335a4b5cccf0
André Viapiana <andreviapiana@gmail.com>
2023-07-02 20:51:25 -0300

    ✨feat: CSS-in-JS - Definindo o tema da aplicação

--- App.tsx
+++ App.tsx
@@ -2,15 +2,20 @@ import { StatusBar } from 'react-native'
 
 import { Groups } from '@screens/Groups'
 
+import { ThemeProvider } from 'styled-components'
+import theme from './src/theme'
+
 export default function App() {
   return (
-    <>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Groups />
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Groups />
+      </>
+    </ThemeProvider>
   )
 }
