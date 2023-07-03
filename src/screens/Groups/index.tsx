d4e8e28ee88ac3b2612add761957423473f776b9
André Viapiana <andreviapiana@gmail.com>
2023-07-03 07:50:44 -0300

    ✨feat: CSS-in-JS - Listando Grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,21 @@ import { Container } from './styles'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Hightlight'
 import { GroupCard } from '@components/GroupCard'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Teste'])
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
