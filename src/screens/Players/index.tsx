8e856649121e8e8787d71d392171a217f5874f56
André Viapiana <andreviapiana@gmail.com>
2023-07-03 09:39:45 -0300

    ✨feat: CSS-in-JS - Componente Filter - Listando os Filtros

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,10 +3,15 @@ import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
-import { Container, Form } from './styles'
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles'
 
 export function Players() {
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState([])
+
   return (
     <Container>
       <Header showBackButton />
@@ -22,7 +27,22 @@ export function Players() {
         <ButtonIcon icon="add" />
       </Form>
 
-      <Filter title="Time A" isActive />
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   )
 }
