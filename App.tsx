034321a0b23805525b94384ba2c83eee3f5e2b83
André Viapiana <andreviapiana@gmail.com>
2023-07-02 21:37:08 -0300

    ✨feat: CSS-in-JS - Fontes Personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'react-native'
+/* eslint-disable camelcase */
+import { StatusBar, ActivityIndicator } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import theme from './src/theme'
 
 import { Groups } from '@screens/Groups'
 
-import { ThemeProvider } from 'styled-components'
-import theme from './src/theme'
-
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
       <>
@@ -14,7 +22,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        <Groups />
+        {fontsLoaded ? <Groups /> : <ActivityIndicator />}
       </>
     </ThemeProvider>
   )
